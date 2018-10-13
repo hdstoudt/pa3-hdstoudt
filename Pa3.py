97916617edbe92dfe999ac2a89c7e2d9b9e2372b
Hannah Stoudt <hdstoudt@loyola.edu>
2018-10-13 17:17:54 -0400

    Working on code need to check algorithm

--- Pa3.py
+++ Pa3.py
@@ -5,22 +5,35 @@ def menu():
 
 
 def error_check_menu():
-    if menu_choice = "football":
+    if menu_choice == "football":
         return true
-    elif menu_choice = "quidditch":
+    elif menu_choice == "quidditch":
         return true
-    elif menu_choice = "gymnast":
+    elif menu_choice == "gymnast":
         return true
     else:
         return false
 
 
-def football():
-
-
 def interceptions():
-    interceptions = int(input("Please enter your number of interceptions."))
+    interceptions = int(input("Please enter your number of interceptions made."))
     return interceptions
+
+def completions():
+    completions = int(input("Please enter your number of completions made."))
+    return completions
+
+def attempts():
+    attempts = int(input("Please enter your number of attempts made."))
+    return attempts
+
+def passing_yards():
+    passing_yards = int(input("Please enter your number of passing yards made."))
+    return passing_yards
+
+def touchdown_passes():
+    touchdown_passes = int(input("Please enter your number of touchdown passes made."))
+    return touchdown_passes
 
 # Function Name: football
 # Parameters: interceptions, completions, attempts, passing_yards, touchdown_passes
@@ -34,3 +47,13 @@ def interceptions():
 # 6. Calculation_QB: "100 * [5(completions/attempts – 0.3) + 0.25(passing_yards/attempts-3) + 20(touchdown_passes/attempts)
 # + 2.375 – (25 * interceptions/attempts)]/6"
 # 7. Return Calculation_QB
+
+def football_cost():
+    interceptions = interceptions()
+    completion = completions()
+    attempts = attempts()
+    passing_yards = passing_yards()
+    touchdown_passes = touchdown_passes()
+    football_cost = (100 * [5 (completion / attempts – 0.3) + 0.25(passing_yards / attempts - 3) + 20 (touchdown_passes
+                    / attempts) + 2.375 – (25 * interceptions / attempts)] / 6)
+    return football_cost
