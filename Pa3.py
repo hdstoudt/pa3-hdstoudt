1c4d5870b45f9326d3cacb518ba592c244602e48
Hannah Stoudt <hdstoudt@loyola.edu>
2018-10-13 16:58:09 -0400

    Working on code need to check algorithm

--- Pa3.py
+++ Pa3.py
@@ -1,5 +1,15 @@
+def menu():
+    print("What type of sport: football, quidditch, gymnast:")
+    sport = input("Please enter a valid sport type.")
+    return menu_choice
 
 
-
-
-
+def error_check_menu():
+    if menu_choice = "football":
+        return true
+    elif menu_choice = "quidditch":
+        return true
+    elif menu_choice = "gymnast":
+        return true
+    else:
+        return false
