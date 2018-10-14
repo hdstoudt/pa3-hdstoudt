4230f1cf94289d8e63eb1d0040f92da7bae55f60
Hannah Stoudt <hdstoudt@loyola.edu>
2018-10-13 22:12:22 -0400

    Working on code need to check algorithm

--- Pa3.py
+++ Pa3.py
@@ -35,25 +35,42 @@ def touchdown_passes():
     touchdown_passes = int(input("Please enter your number of touchdown passes made."))
     return touchdown_passes
 
-# Function Name: football
-# Parameters: interceptions, completions, attempts, passing_yards, touchdown_passes
-# Return: Calculation of Parameters
-# Algorithm:
-# 1. Ask user to input Parameter: "interceptions"
-# 2. Ask user to input Parameter: "completions"
-# 3. Ask user to input Parameter: "attempts"
-# 4. Ask user to input Parameter: "passing_yards"
-# 5. Ask user to input Parameter: "touchdown_passes"
-# 6. Calculation_QB: "100 * [5(completions/attempts – 0.3) + 0.25(passing_yards/attempts-3) + 20(touchdown_passes/attempts)
-# + 2.375 – (25 * interceptions/attempts)]/6"
-# 7. Return Calculation_QB
 
-def football_cost():
+def football():
     interceptions = interceptions()
     completion = completions()
     attempts = attempts()
     passing_yards = passing_yards()
     touchdown_passes = touchdown_passes()
-    football_cost = (100 * [5 (completion / attempts – 0.3) + 0.25(passing_yards / attempts - 3) + 20 (touchdown_passes
-                    / attempts) + 2.375 – (25 * interceptions / attempts)] / 6)
-    return football_cost
+    football_score = (100 * [5 (completion / attempts - 0.3) + 0.25(passing_yards / attempts - 3) + 20 (touchdown_passes
+                    / attempts) + 2.375 - (25 * interceptions / attempts)] / 6)
+    return football_score
+
+
+
+
+#Function Name: Main
+#Parameters: None
+#Return: None
+#Algorithm:
+#1. Tell the user the purpose of the program: "This program will calculate various sports statistics for the user, based
+#on the user’s choice of sport."
+#2. Call menu
+#3. Call error_check_menu
+#4. If error_check_menu is equal to "false":
+    #a. Output error message: "Sorry your answer is invalid, please try again."
+   # b. Go back to Step 2 #In main function
+#5. If menu_choice is equal to "football":
+    #a. Call football
+    #b. Output: ("Your football QB rating is", Calculation_QB)
+    #c. If Calculation_QB is equal to 158.3
+       # i.Output: "You are a perfect passer."
+    #d. Otherwise:
+     #   a. Output: "Sorry, you are not considered a perfect passer."
+#6. Otherwise/If menu_choice is equal to "quidditch":
+  #  a. Call quidditch
+ #   b. Output: ("Your score for Quidditch is", Calculation_Quid)
+#7. Otherwise:
+  #  a. Call gymnast
+  #  b. Output: ("Your final score is", Calculation_Gym)
+#8. Output: "Thank you for using this program."
