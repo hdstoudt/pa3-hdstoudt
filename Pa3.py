e77c97c4fed77d7094bac5cadb47f8af1926f890
Hannah Stoudt <hdstoudt@loyola.edu>
2018-10-14 14:47:35 -0400

    Working on code need to check algorithm

--- Pa3.py
+++ Pa3.py
@@ -10,7 +10,7 @@ def menu():
     return menu_choice
 
 
-def error_check_menu():
+def error_check_menu(menu_choice):
     if menu_choice == "football":
         return true
     elif menu_choice == "quidditch":
@@ -20,14 +20,12 @@ def error_check_menu():
     else:
         return false
 
-#Function Name: int_check
-#Parameters: value
-#Return: true or false
-Algorithm:
-1. If int_check is int:
-    a. Return true
-2. Otherwise:
-    a. Return False
+
+def int_check(int):
+    if int_check == int
+        return true
+    else:
+        return false
 
 
 def football(interceptions, completions, attempts, passing_yards, touchdown_passes):
@@ -61,11 +59,11 @@ def touchdown_passes():
     touchdown_passes = int(input("Please enter your number of touchdown passes made."))
     return touchdown_passes
 
-def quidditch():
-    goal = goals()
+def quidditch(goals, snitch):
+    goals = goals()
     int_check()
     if int_check == "true":
-        quidditch_score = 10(goal)
+        quidditch_score = 10(goals)
     else:
         print("Error! Invalid Answer.")
         return 0
@@ -80,12 +78,71 @@ def quidditch():
     return quidditch_score
 
 def goals():
-    goal = int(input("Please enter your number of goals made."))
-    return goal
+    goals = int(input("Please enter your number of goals made."))
+    return goals
 
 def snitch():
     snitch = int(input("Did you catch the golden snitch? yes or no?"))
     return snitch
+
+def gymnast(difficulty, execution1, execution2, execution3, execution4, execution5):
+
+Function Name: gymnast
+Parameters: difficulty, execution1, execution2, execution3, execution4, execution5
+Return: Calculation of Parameters
+Algorithm:
+1. Ask user to input Parameter: "difficulty"
+2. Call int_check to check for valid interger
+3. If valid:
+    a. Continue on
+4. Otherwise:
+    a. Output: "Error! Invalid Answer."
+    b. return 0
+5. Ask user to input Parameter: "execution1"
+6. Call int_check to check for valid interger
+7. If valid:
+    a. Continue on
+8. Otherwise:
+    a. Output: "Error! Invalid Answer."
+    b. return 0
+9. Ask user to input Parameter: "execution2"
+10. Call int_check to check for valid interger
+11. If valid:
+    a. Continue on
+12. Otherwise:
+    a. Output: "Error! Invalid Answer."
+    b. return 0
+13. Ask user to input Parameter: "execution3"
+14. Call int_check to check for valid interger
+15. If valid:
+    a. Continue on
+16. Otherwise:
+    a. Output: "Error! Invalid Answer."
+    b. return 0
+17. Ask user to input Parameter: "execution4"
+18. Call int_check to check for valid interger
+19. If valid:
+    a. Continue on
+20. Otherwise:
+    a. Output: "Error! Invalid Answer."
+    b. return 0
+21. Ask user to input Parameter: "execution5"
+22. Call int_check to check for valid interger
+23. If valid:
+    a. Continue on
+24. Otherwise:
+    a. Output: "Error! Invalid Answer."
+    b. return 0
+25. Check for min_value for calculation
+26. Check for max-value for calculation
+27. Calculate average_executions: "((execution1 + execution2 + execution3 + execution4 + execution5) - (min_value +
+max_value)) / 5 "
+28. Calculate_Gym = "(difficulty) + (average_executions)"
+29. Return Calculation_Gym
+
+def difficulty():
+    difficulty = int(input("Please enter difficulty score."))
+    return difficulty
 
 
 def main():
@@ -94,7 +151,7 @@ def main():
     error_check_menu = error_check_menu()
     if error_check_menu == false
         print("Sorry your answer is invalid, please try again.")
-        ## go back to menu step 2
+        ## Go back to menu_choice ##
     if menu_choice == "football"
         football()
         print("Your football QB rating is", football_score)
